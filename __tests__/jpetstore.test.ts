950ad801bcdf2cc4a8fbf96bc3633b40ced67302
adrianpothuaud <adrianpothuaud@protonmail.com>
2022-08-26 21:50:15 +0200

    test(sample): add sample test against jpetstore

---
+++ __tests__/jpetstore.test.ts
@@ -0,0 +1,29 @@
+import { getFirefoxDriver } from '../src'
+import { IEnhancedDriver } from '../src/interfaces'
+
+describe('Sign In', () => {
+  let driver: IEnhancedDriver
+
+  beforeAll(async () => {
+    driver = await getFirefoxDriver(true)
+  }, 10000)
+
+  afterAll(async () => {
+    await driver.quit()
+  }, 10000)
+
+  it('open demo application and enter the store', async () => {
+    await driver.navTo('https://petstore.octoperf.com/')
+    await driver.click('a[txt:Enter the Store]')
+  }, 30000)
+
+  it('goes to sign in page through the link on the header', async () => {
+    await driver.click('div[#:MenuContent]->a[txt:Sign In]')
+  }, 30000)
+
+  it('fills login form', async () => {
+    await driver.fill('input[n:username]', 'username')
+    await driver.fill('input[n:password]', 'password')
+    await driver.click('input[n:signon]')
+  }, 20000)
+})
