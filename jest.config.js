e9ab7d37e4efe3ad1db06efe592b93fc66ff2b65
adrianpothuaud <adrianpothuaud@protonmail.com>
2022-08-26 23:02:09 +0200

    chore(setup): added webdriver manager and lcov reports

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,15 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/*.ts',
+    '!src/env.ts',
+    '!src/index.ts',
+    '!**/node_modules/**',
+    '!__tests__/**',
+  ],
+  coverageReporters: ['lcov'],
   preset: 'ts-jest',
   runner: 'groups',
   testEnvironment: 'node',
